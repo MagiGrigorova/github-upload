610768010db8c1718f0fa73197c8523911e0049e
MagiGrigorova <72687596+MagiGrigorova@users.noreply.github.com>
2021-03-05 18:00:44 +0200

    second test to see if changes made locally are reflected in the repository

--- src/Nums1To10.java
+++ src/Nums1To10.java
@@ -1,7 +1,7 @@
 public class Nums1To10 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 1);
         }
     }
 }
